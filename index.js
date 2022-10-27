361795b62878c8ce59f99e2a7214fa39df6af26e
Juliano Alfredo <juliano.alfredo86@gmail.com>
2022-10-27 09:23:19 -0300

    copying

--- index.js
+++ index.js
@@ -1,67 +1,90 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const shortid = require('shortid')
-const dotenv = require('dotenv').config()
-var app = express()
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const validURL = require('valid-url');
+const shortID = require('shortid');
+require('dotenv').config();
 
-// Basic Configuration
+const app = express();
 const port = process.env.PORT || 3000;
 
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// MongoDB and mongoose connect
+mongoose.connect('mongodb+srv://julianoalfredo:jujuba09@cluster0.dszyxym.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.post('/api/shorturl/new', async function(req, res){
-  const {url} = req.body
-  const uid = shortid.generate()
+// Database schema
+const urlSchema = new mongoose.Schema({
+  originalURL: String,
+  shortURL: String,
+});
+
+const URL = mongoose.model('URL', urlSchema);
+
+// App middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', express.static(`${process.cwd()}/public`));
+app.get('/', function (req, res) {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+// Response for POST request
+app.post('/api/shorturl/new', async (req, res) => {
+  const { url } = req.body;
+  const shortURL = shortID.generate();
   console.log(validURL.isUri(url));
   if (validURL.isWebUri(url) === undefined) {
     res.json({
       error: 'invalid url',
     });
-  } else{
-    console.log(url)
-    client.connect( async err =>{
-       if (err) throw err
-       await client.db('url-db').collection('url-config').insertOne({
-         "url": `${url}`,
-         "short_id": `${uid}`,
-       })
-       res.send({
-         "url": `${url}`,
-         "short_id": `${uid}`,
-       })
-      })
+  } else {
+    try {
+      let findOne = await URL.findOne({
+        originalURL: url,
+      });
+      if (findOne) {
+        res.json({
+          original_url: findOne.originalURL,
+          short_url: findOne.shortURL,
+        });
+      } else {
+        findOne = new URL({
+          originalURL: url,
+          shortURL,
+        });
+        await findOne.save();
+        res.json({
+          original_url: findOne.originalURL,
+          short_url: findOne.shortURL,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json('Server error..');
+    }
   }
-   
-})
-app.get("/api/shorturl/:shorturl", async function(req, res){
-  client.connect(async err =>{
-      if(err) throw err
-      await client.db("url-db").collection("url-config").find({}).toArray(async function(err, result){
-          if(err) throw err;
-          result.forEach((obj) =>{
-              if(obj.short_id == req.params.shorturl){
-                  res.redirect(obj.url)
-              } else{
-                  console.log("PASSED")
-              }
-          })
-      })
-  })
-})
+});
 
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+// Redirect shortened URL to Original URL
+app.get('/api/shorturl/:shortURL?', async (req, res) => {
+  try {
+    const urlParams = await URL.findOne({
+      shortURL: req.params.shortURL,
+    });
+    if (urlParams) {
+      return res.redirect(urlParams.originalURL);
+    }
+    return res.status(404).json('No URL found');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Server error..');
+  }
+});
+// Listens for connections
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
